2506ab275453273c15f3e5768d516093eb9ae90b
Gh0ul5lay3r-91 <brennan.eoin91@gmail.com>
2021-11-19 15:33:05 +0000

    Add Error display modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,24 @@ document.getElementById("submit").addEventListener("click", e => postForm(e));
 async function postForm(e){
     const form = new FormData(document.getElementById("checksform"));
 
-    const await = fetch(API_URL, {
+    const response = await fetch(API_URL, {
                         method: "POST",
                         headers: {
                             "Authorization": API_KEY,
         },
-                        body: form;
+                        body: form,
     })
+
+    const data = await response.json();
+
+    if(response.ok){
+        displayErrors(data);
+    }else {
+        throw new Error(data.error);
+    }
+
 }
+
 async function getStatus(e) {
 
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -30,6 +40,25 @@ async function getStatus(e) {
         throw new Error(data.error);
     }
 
+}
+
+function displayErrors(data){
+    let heading = `JSHint Results fo ${data.file}`;
+
+    if(data.total_errors === 0){
+        results = `<div class="no_errors>No errors report!</div>`;
+    }else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for(let error of data.error_list){
+            results += `<div>At the line <span class="line">${error.line}</span>, `;
+            results += `column <span class=column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
